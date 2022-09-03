e9f30c6437230ed7e78a8daab00ab5ff3c1e1673
iqfareez <iqmal3@outlook.com>
2022-09-03 12:27:57 +0800

    :sparkles: Exposes zone endpoint

---
+++ pages/api/zones/[code].js
@@ -0,0 +1,21 @@
+import {loadZones} from "../../../lib/load-json-db";
+
+export default async function handler(req, res) {
+  // https://vercel.com/guides/loading-static-file-nextjs-api-route
+  const zones = await loadZones();
+
+  const {code} = req.query;
+
+  const filteredZones = zones.filter(zone => zone.jakimCode.toLowerCase().includes(code));
+
+  if (!filteredZones.length) {
+    res.status(404).json({
+      error: `No zone found for code: ${code}`
+    });
+    return;
+  }
+
+  // return all the solat times data in JSON
+  res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
+  res.status(200).json(filteredZones);
+}
---
+++ pages/api/zones/index.js
@@ -0,0 +1,10 @@
+import {loadZones} from "../../../lib/load-json-db";
+
+export default async function handler(req, res) {
+  // https://vercel.com/guides/loading-static-file-nextjs-api-route
+  const zones = await loadZones();
+
+  // return all the solat times data in JSON
+  res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
+  res.status(200).json(zones);
+}
