dcde439be8a7620458612ca0adff14740059dbdd
iqfareez <iqmal3@outlook.com>
2022-09-09 15:24:45 +0800

    :bug: Fixed build error Buffer error

--- lib/load-json-db.ts
+++ lib/load-json-db.ts
@@ -2,25 +2,19 @@ import path from "path";
 import fsPromises from "fs/promises";
 
 export async function loadLog() {
-
   const filePath = path.join(process.cwd(), 'json/log.json');
-  const jsonData = await fsPromises.readFile(filePath);
+  const jsonData = await fsPromises.readFile(filePath, 'utf-8');
   return JSON.parse(jsonData);
-
 }
 
 export async function loadWaktuSolat() {
-
   const filePath = path.join(process.cwd(), 'json/db.json');
-  const jsonData = await fsPromises.readFile(filePath);
+  const jsonData = await fsPromises.readFile(filePath, 'utf-8');
   return JSON.parse(jsonData);
-
 }
 
 export async function loadZones() {
-
   const filePath = path.join(process.cwd(), 'json/zones.json');
-  const jsonData = await fsPromises.readFile(filePath);
+  const jsonData = await fsPromises.readFile(filePath, 'utf-8');
   return JSON.parse(jsonData);
-
 }
