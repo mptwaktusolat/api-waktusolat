716752e1a26b40312d445b661744f667c1aaddf1
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-04-30 08:54:39 +0800

    :chart_with_upwards_trend: Add vercel anaylytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,19 @@ import '../styles/globals.css'
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }) {
+import type {AppProps } from 'next/app';
+import { Analytics } from "@vercel/analytics/react";
+
+function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
+  );
 }
 
 export default MyApp
