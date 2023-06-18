17cbabd45886ba2330292400ca8f629684fed1b2
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-06-18 22:46:42 +0800

    :mag: Update metatags og image

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,51 +3,54 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-      <div className={styles.container}>
-        <Head>
-          <title>MPT Server</title>
-          <meta
-              name="description"
-              content="Serving prayer time API and some endpoints for MPT internal use"
-          />
-          <meta name={"og:image"} content={"https://res.cloudinary.com/iqfareez-cloud/image/upload/v1687052462/1_r0mixv.png"}/>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>MPT Server</title>
+                <meta
+                    name="description"
+                    content="Serving prayer time API and some endpoints for MPT internal use"
+                />
+                <meta property="og:title" content="MPT Server - Malaysia Prayer Time API"/>
+                <meta property={"og:image"}
+                      content={"https://res.cloudinary.com/iqfareez-cloud/image/upload/v1687052462/1_r0mixv.png"}/>
 
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to <Link href="#">MPT Server!</Link>
-          </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome to <Link href="#">MPT Server!</Link>
+                </h1>
 
-          <p className={styles.description}>
-            <span>Malaysia prayer time API is <b>available</b>! Learn more on <Link className={styles.link} href={"https://waktusolat.iqfareez.com/api"}>docs</Link>.</span>
-          </p>
+                <p className={styles.description}>
+                    <span>Malaysia prayer time API is <b>available</b>! Learn more on <Link className={styles.link}
+                                                                                            href={"https://waktusolat.iqfareez.com/api"}>docs</Link>.</span>
+                </p>
 
-          <div className={styles.textContent}>
-              <span>See all <Link className={styles.link} href="/locations">prayer time zones</Link>.</span>
-          </div>
+                <div className={styles.textContent}>
+                    <span>See all <Link className={styles.link} href="/locations">prayer time zones</Link>.</span>
+                </div>
 
-          <div className={styles.grid}>
-            <Link
-                href="https://play.google.com/store/apps/details?id=live.iqfareez.waktusolatmalaysia"
-                className={styles.card}
-            >
-              <p>MPT App &rarr;</p>
-            </Link>
+                <div className={styles.grid}>
+                    <Link
+                        href="https://play.google.com/store/apps/details?id=live.iqfareez.waktusolatmalaysia"
+                        className={styles.card}
+                    >
+                        <p>MPT App &rarr;</p>
+                    </Link>
 
-            <Link
-                href="https://github.com/iqfareez/mpt-server"
-                className={styles.card}
-            >
-              <p>GitHub &rarr;</p>
-            </Link>
-          </div>
-        </main>
-        <footer className={styles.footer}>
-          <Link href="https://iqfareez.com">© Muhammad Fareez Iqmal</Link>
-        </footer>
-      </div>
-  );
+                    <Link
+                        href="https://github.com/iqfareez/mpt-server"
+                        className={styles.card}
+                    >
+                        <p>GitHub &rarr;</p>
+                    </Link>
+                </div>
+            </main>
+            <footer className={styles.footer}>
+                <Link href="https://iqfareez.com">© Muhammad Fareez Iqmal</Link>
+            </footer>
+        </div>
+    );
 }
