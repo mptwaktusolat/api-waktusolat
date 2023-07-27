d659281497f05f5d55d548a1e683a1975238b19c
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-07-27 23:38:47 +0800

    :children_crossing: Add loading spinner in locations path
    added react-loader-spinner

--- pages/locations.tsx
+++ pages/locations.tsx
@@ -4,6 +4,7 @@ import useSWR from 'swr';
 import Toastify from 'toastify-js'
 import styles from "../styles/Locations.module.css";
 import "toastify-js/src/toastify.css"
+import {RotatingTriangles} from "react-loader-spinner";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -39,46 +40,58 @@ export default function Locations() {
     const zones: Map<String, any> = groupBy(data ?? [], 'negeri');
 
     return (
-        <div className={styles.container}>
+        <>
             <Head>
                 <title>Locations - MPT Server</title>
                 <meta name="description" content="JAKIM zones list"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
+            <div className={styles.container}>
+                <div className={styles.description}>
+                    <h1>All locations. Based on JAKIM.</h1>
+                    <h3>Also available in <Link className={styles.link} href="/api/zones">JSON</Link> format</h3>
+                    {(!data) &&
+                        <div>
+                            <RotatingTriangles
+                                visible={true}
+                                height="80"
+                                width="80"
+                                ariaLabel="rotating-triangels-loading"
+                                wrapperStyle={{}}
+                                wrapperClass="rotating-triangels-wrapper"
+                            />
+                            <p>Loading...</p>
+                        </div>
+                    }
+                </div>
+                {
+                    Object.entries(zones).map(([negeri, data]) => (
+                        <div key={negeri} style={{paddingBottom: 15 + 'px'}}>
+                            <h2>{negeri}</h2>
+                            {
+                                data.map(zone => (
+                                    <div key={zone.jakimCode} className={styles.zoneItem}>
+                                        <code className={styles.zonecode}
+                                              onClick={() => copyToClipBoard(zone.jakimCode)}>
+                                            {zone.jakimCode}
+                                        </code> - {zone.daerah}
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    ))
+                }
 
-            <div className={styles.description}>
-                <h1>All locations. Based on JAKIM.</h1>
-                <h3>Also available in <Link className={styles.link} href="/api/zones">JSON</Link> format</h3>
-            </div>
-            {
-                Object.entries(zones).map(([negeri, data]) => (
-                    <div key={negeri} style={{paddingBottom: 15 + 'px'}}>
-                        <h2>{negeri}</h2>
-                        { data ? (
-                            data.map(zone => (
-                                <div key={zone.jakimCode} className={styles.zoneItem}>
-                                    <code className={styles.zonecode} onClick={() => copyToClipBoard(zone.jakimCode)}>
-                                        {zone.jakimCode}
-                                    </code> - {zone.daerah}
-                                </div>
-                            ))
-                        ) : (
-                            <div>Loading...</div>
-                        )
-                        }
-                    </div>
-                ))
-            }
-
-            <div style={{paddingBottom: "1rem"}}></div>
-            <div className={styles.footer}>
+                <div style={{paddingBottom: "1rem"}}></div>
+                <div className={styles.footer}>
                 <span>This data is updated on best-effort basis. If you find the data above incorrect/outdated, kindly open an issue <Link
                     className={styles.link}
                     href={"https://github.com/mptwaktusolat/jakim-zones-grabber/issues"}>here</Link>.</span>
 
-                <br/>
-                <Link className={styles.link} href={"/"} >Back home</Link>
+                    <br/>
+                    <Link className={styles.link} href={"/"}>Back home</Link>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
