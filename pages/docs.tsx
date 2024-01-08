2faf87ff0d7157e1dd3b3c529bf45cbe3802d65d
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2024-01-08 18:53:03 +0000

    :see_no_evil: Hide "Powered by Scalar"

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -1,9 +1,23 @@
 import { ApiReference as VueComponent } from '@scalar/api-reference'
+import { useEffect } from 'react';
 import { applyVueInReact } from 'veaury'
 
 const ApiReference = applyVueInReact(VueComponent)
 
 function App() {
+
+    useEffect(()=> {
+        // Hiding scalar attributionn muehehheheh
+        const attributionElementMobile = document.querySelector("#__next > div > div.scalar-api-reference.references-layout.references-sidebar.scalar-api-references-standalone-mobile._reset_1ffu1_3 > aside > div > div > div.darklight-reference > a");
+        const attributionElement = document.querySelector("#__next > div > div.scalar-api-reference.references-layout.references-sidebar._reset_1ffu1_3 > aside > div > div > div.darklight-reference > a");
+        if (attributionElementMobile != null) {
+            attributionElementMobile.innerHTML = ""   
+        }
+        if (attributionElement != null) {
+            attributionElement.innerHTML = ""   
+        }
+    
+    });
     const configuration = {
         spec: {
             url: 'https://raw.githubusercontent.com/mptwaktusolat/open-api-spec/main/mpt-server-api.yaml'
