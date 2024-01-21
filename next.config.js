9af166a3f79fa4b955b210511327ef0d3ca11c09
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2024-01-21 15:51:38 +0000

    :lock: Make api routes allow all origins

--- next.config.js
+++ next.config.js
@@ -8,10 +8,16 @@ const nextConfig = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // allow all origin
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+        ]
+      },
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // allow all origin
+          { key: "Access-Control-Allow-Methods", value: "GET" },
         ]
       }
     ]
