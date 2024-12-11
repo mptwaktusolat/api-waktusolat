7e7fbdb0cac7625b8e832c30b7b0f9eca0ebcda8
iqfareez <60868965+iqfareez@users.noreply.github.com>
2024-12-12 05:55:44 +0800

    :lipstick: (Dark mode #11) Use media strategy instead of class strategy
    https://tailwindcss.com/docs/dark-mode
    
    - Fix incorrect background color
    - Border color

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,7 +73,7 @@ export default function Home() {
           </div>
         </section>
       </main>
-      <footer className="flex fixed bottom-0 min-w-full items-center h-16 px-4 border-t md:px-6 mt-auto">
+      <footer className="flex fixed bottom-0 min-w-full items-center h-16 px-4 border-t md:px-6 mt-auto dark:border-gray-600">
         <p className="text-sm text-gray-500 dark:text-gray-400">
           © 2024{" "}
           <a
