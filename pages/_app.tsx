949567157a095b71c9800c34c067ce6ed0164982
iqfareez <60868965+iqfareez@users.noreply.github.com>
2025-02-21 06:11:10 +0800

    :chart_with_upwards_trend: Migrate from Vercel Analytics to Umami analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import '../styles/globals.css'
-
-import type {AppProps } from 'next/app';
-import { Analytics } from "@vercel/analytics/react";
+import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <>
-        <Analytics />
-        <Component {...pageProps} />
-      </>
-  );
+    <>
+      <Script
+        src="https://umami.iqfareez.com/script.js"
+        data-website-id="11df7d3a-fb7e-4bbd-aacb-222515ea7b32"
+        strategy="lazyOnload"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
